d7349f2c0848acc01717c798316100928e78705c
Vossup <odin.lodemel@gmail.com>
2022-12-14 17:16:29 +0100

    idk

--- client/src/clientTest.tsx
+++ client/src/clientTest.tsx
@@ -8,72 +8,83 @@ import { NavLink } from 'react-router-dom';
  *
  * Students: this slightly more complex component is not part of curriculum.
  */
-export class Alert extends Component {
-  alerts: { id: number; text: ReactNode; type: string }[] = [];
-  nextId: number = 0;
+// export class Alert extends Component {
+//   alerts: { id: number; text: ReactNode; type: string }[] = [];
+//   nextId: number = 0;
 
+//   render() {
+//     return (
+//       <div>
+//         {this.alerts.map((alert, i) => (
+//           <div
+//             key={alert.id}
+//             className={'alert alert-dismissible alert-' + alert.type}
+//             role="alert"
+//           >
+//             {alert.text}
+//             <button
+//               type="button"
+//               className="btn-close btn-sm"
+//               onClick={() => this.alerts.splice(i, 1)}
+//             />
+//           </div>
+//         ))}
+//       </div>
+//     );
+//   }
+
+//   /**
+//    * Show success alert.
+//    */
+//   static success(text: ReactNode) {
+//     // To avoid 'Cannot update during an existing state transition' errors, run after current event through setTimeout
+//     setTimeout(() => {
+//       let instance = Alert.instance(); // Get rendered Alert component instance
+//       if (instance) instance.alerts.push({ id: instance.nextId++, text: text, type: 'success' });
+//     });
+//   }
+
+//   /**
+//    * Show info alert.
+//    */
+//   static info(text: ReactNode) {
+//     // To avoid 'Cannot update during an existing state transition' errors, run after current event through setTimeout
+//     setTimeout(() => {
+//       let instance = Alert.instance(); // Get rendered Alert component instance
+//       if (instance) instance.alerts.push({ id: instance.nextId++, text: text, type: 'info' });
+//     });
+//   }
+
+//   /**
+//    * Show warning alert.
+//    */
+//   static warning(text: ReactNode) {
+//     // To avoid 'Cannot update during an existing state transition' errors, run after current event through setTimeout
+//     setTimeout(() => {
+//       let instance = Alert.instance(); // Get rendered Alert component instance
+//       if (instance) instance.alerts.push({ id: instance.nextId++, text: text, type: 'warning' });
+//     });
+//   }
+
+//   /**
+//    * Show danger alert.
+//    */
+//   static danger(text: ReactNode) {
+//     // To avoid 'Cannot update during an existing state transition' errors, run after current event through setTimeout
+//     setTimeout(() => {
+//       let instance = Alert.instance(); // Get rendered Alert component instance
+//       if (instance) instance.alerts.push({ id: instance.nextId++, text: text, type: 'danger' });
+//     });
+//   }
+// }
+
+export class Count extends Component {
+  count: number = 0;
   render() {
     return (
-      <div>
-        {this.alerts.map((alert, i) => (
-          <div
-            key={alert.id}
-            className={'alert alert-dismissible alert-' + alert.type}
-            role="alert"
-          >
-            {alert.text}
-            <button
-              type="button"
-              className="btn-close btn-sm"
-              onClick={() => this.alerts.splice(i, 1)}
-            />
-          </div>
-        ))}
-      </div>
+      <>
+        <button onClick={() => this.count++}>{this.count}</button>
+      </>
     );
-  }
-
-  /**
-   * Show success alert.
-   */
-  static success(text: ReactNode) {
-    // To avoid 'Cannot update during an existing state transition' errors, run after current event through setTimeout
-    setTimeout(() => {
-      let instance = Alert.instance(); // Get rendered Alert component instance
-      if (instance) instance.alerts.push({ id: instance.nextId++, text: text, type: 'success' });
-    });
-  }
-
-  /**
-   * Show info alert.
-   */
-  static info(text: ReactNode) {
-    // To avoid 'Cannot update during an existing state transition' errors, run after current event through setTimeout
-    setTimeout(() => {
-      let instance = Alert.instance(); // Get rendered Alert component instance
-      if (instance) instance.alerts.push({ id: instance.nextId++, text: text, type: 'info' });
-    });
-  }
-
-  /**
-   * Show warning alert.
-   */
-  static warning(text: ReactNode) {
-    // To avoid 'Cannot update during an existing state transition' errors, run after current event through setTimeout
-    setTimeout(() => {
-      let instance = Alert.instance(); // Get rendered Alert component instance
-      if (instance) instance.alerts.push({ id: instance.nextId++, text: text, type: 'warning' });
-    });
-  }
-
-  /**
-   * Show danger alert.
-   */
-  static danger(text: ReactNode) {
-    // To avoid 'Cannot update during an existing state transition' errors, run after current event through setTimeout
-    setTimeout(() => {
-      let instance = Alert.instance(); // Get rendered Alert component instance
-      if (instance) instance.alerts.push({ id: instance.nextId++, text: text, type: 'danger' });
-    });
   }
 }
--- client/test/clientTest.test.tsx
+++ client/test/clientTest.test.tsx
@@ -1,33 +1,49 @@
 import * as React from 'react';
-import { Alert } from '../src/clientTest';
+import { /*Alert,*/ Count } from '../src/clientTest';
 import { shallow } from 'enzyme';
 
-describe('Alert tests', () => {
-  test('No alerts initially', () => {
-    const wrapper = shallow(<Alert />);
+// describe('Alert tests', () => {
+//   test.skip('No alerts initially', () => {
+//     const wrapper = shallow(<Alert />);
 
-    expect(wrapper.matchesElement(<div></div>)).toEqual(true);
+//     expect(wrapper.matchesElement(<div></div>)).toEqual(true);
+//   });
+
+//   test.skip('Show alert message', (done) => {
+//     const wrapper = shallow(<Alert />);
+
+//     Alert.danger('test');
+
+//     // Wait for events to complete
+//     setTimeout(() => {
+//       expect(
+//         wrapper.matchesElement(
+//           <div>
+//             <div>
+//               test
+//               <button />
+//             </div>
+//           </div>
+//         )
+//       ).toEqual(true);
+
+//       done();
+//     });
+//   });
+// });
+
+describe('Count tests', () => {
+  test('Initial count is 0', () => {
+    const wrapper = shallow(<Count />);
+
+    expect(wrapper.containsMatchingElement(<button>{0}</button>)).toEqual(true);
   });
 
-  test('Show alert message', (done) => {
-    const wrapper = shallow(<Alert />);
+  test('Increment count', () => {
+    const wrapper = shallow(<Count />);
 
-    Alert.danger('test');
+    wrapper.find('button').simulate('click');
 
-    // Wait for events to complete
-    setTimeout(() => {
-      expect(
-        wrapper.matchesElement(
-          <div>
-            <div>
-              test
-              <button />
-            </div>
-          </div>
-        )
-      ).toEqual(true);
-
-      done();
-    });
+    expect(wrapper.containsMatchingElement(<button>{1}</button>)).toEqual(true);
   });
 });
