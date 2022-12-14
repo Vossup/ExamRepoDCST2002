b9f570261fc3ce1df8c09cd218d848ee08e90728
Vossup <odin.lodemel@gmail.com>
2022-12-14 17:07:33 +0100

    set up last parts for testing api

--- server/src/API.ts
+++ server/src/API.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 
-/**
- * Express router containing task methods.
- */
+//test can be done with postman towards path localhost:3000/api/v2/<path>
+
 const router = express.Router();
 
+let tasks = [ { id: 1, name: 'Task 1' }, { id: 2, name: 'Task 2' }, { id: 3, name: 'Task 3' } ];
+
 router.get('/tasks', (_request, response) => {
+  //send back a list of tasks
+  response.send(tasks);
 });
 
 router.get('/tasks/:id', (request, response) => {
